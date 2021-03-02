c5ad5c928d176cd0e48ea4739a150ec92adb0de7
peter21476 <peter.charitopoulos@yahoo.com>
2021-03-01 21:00:31 -0500

    new update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,6 +4,7 @@ module.exports = function(app) {
     app.use(
         createProxyMiddleware("/events", {
             target: "https://api.bizzabo.com/api",
+            secure: true,
             changeOrigin: true
         })
     );
